64c565a61c5a4bcf7de5d60ac2cafac6a55027e7
fjsmu <rikugyouza@gmail.com>
2017-06-05 20:47:49 +0900

    first commit

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 移動処理
+        Move();
+    }
+
+    //移動関数
+    void Move()
+    {
+        Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = position;
+    }
+}
---
+++ Assets/Script/Splash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour {
+    private ParticleSystem particle;
+
+	// Use this for initialization
+	void Start () {
+        particle = this.GetComponent<ParticleSystem>();
+        particle.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = position;
+            particle.Play();
+        }   
+		
+	}
+}
