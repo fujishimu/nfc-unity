5a094ff344aefac32d627b6f8485f8a2da935133
fjsmu <rikugyouza@gmail.com>
2017-06-05 22:16:29 +0900

    エフェクトをprefab化

---
+++ Assets/Script/GameMng.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMng : MonoBehaviour {
+
+    GameObject splash;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject splash = (GameObject)Resources.Load("Prefabs/Splash");
+            Instantiate(splash, new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Quaternion.identity);
+        }
+
+    }
+}
