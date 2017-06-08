b298a59033a2751630b15ee57950a43b45ce6639
fjsmu <rikugyouza@gmail.com>
2017-06-09 02:21:44 +0900

    Fadeを実装中

---
+++ Assets/Script/FadeMng.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class FadeMng : MonoBehaviour
+{
+
+    // Materialを保持するようにする
+    [SerializeField]
+    private Material _mat;
+
+    private int winW = Screen.width;
+    private int winH = Screen.height;
+
+    private void Start()
+    {
+        transform.position = new Vector3(winW / 2, winH / 2);
+
+        var mesh = new Mesh();
+        mesh.vertices = new Vector3[] {
+            new Vector3 (winW, winH),
+            new Vector3 (winW, -winH),
+            new Vector3 (-winW, -winH),
+            new Vector3 (-winW, winH)
+        };
+        mesh.triangles = new int[] {
+            0, 1, 2,
+            0, 2, 3,
+        };
+
+        var filter = GetComponent<MeshFilter>();
+        filter.sharedMesh = mesh;
+
+        // MeshRendererからMaterialにアクセスし、Materialをセットする
+        var renderer = GetComponent<MeshRenderer>();
+        renderer.material = _mat;
+    }
+}
---
+++ Assets/Script/StartButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+    
+    public void GameStart()
+    {
+    GameObject fade = (GameObject)Resources.Load("Prefabs/Fade");
+    Instantiate(fade);
+
+    //SceneManager.LoadScene("scene/main");
+        
+    }
+}
